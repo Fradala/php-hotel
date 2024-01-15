36a1c8d928cc7348812a04dff3d6c0696eb436ce
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2024-01-15 15:17:23 +0100

    messi bene a schermo ed inserito bootstrap

--- index.php
+++ index.php
@@ -43,17 +43,36 @@
 ?>
 
 
-<h1>
-    Hotel
-</h1>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
 
-<?php 
-    foreach ( $hotels as $hotel ) {
-        echo $hotel['name'] . ' , ', $hotel['description'] . ' , ',  
-        $hotel['parking'] . ' ,  ',  $hotel['vote'] . ' ,  ',
-        $hotel['distance_to_center'] . ' - ';
-    }
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+    <h1>
+        Hotel
+    </h1>
 
 
-?>
+    <ul>
+        <?php foreach ( $hotels as $hotel ) { ?>
+            <li>
+                <p>
+                    <?php echo $hotel['name'] . ' , ', $hotel['description'] . ' , ',  
+                    $hotel['parking'] . ' ,  ',  $hotel['vote'] . ' ,  ',
+                    $hotel['distance_to_center']; ?>
+                </p>
+
+            </li>
+        <?php } ?>
+    </ul>
+
+    
+</body>
+</html>
 
